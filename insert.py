96917dc4bdb8c0dcd2c9fa23b7afa20afa0e000d
Luan <luanbfarias@outlook.com>
2023-06-16 21:00:06 -0300

    Versão atualizada do Insert

--- insert.py
+++ insert.py
@@ -3,34 +3,54 @@ from operacoesbd import *
 
 connect = abrirBancoDados("localhost", "root", "root", "bdouvidoria")
 
+
 def adicionarPorTipo():
-    tipo = input ('Digite o tipo de feedback desejado: ').capitalize()
-    
-    if not tipo == ["Reclamação", "Sugestão", "Elogio"]:
-        print ("Manifestação invalida!")
+    tipo = input("Digite o tipo de feedback desejado: ").capitalize()
+
+    if not tipo in ["Reclamação", "Sugestão", "Elogio"]:
+        print("Manifestação invalida!")
     else:
-        if tipo == 'Elogio':
-            titulo = input('Insira o titulo do seu elogio :): ')
+        if tipo == "Elogio":
+            titulo = input("Insira o titulo do seu elogio: ")
             print()
             time.sleep(0.5)
-           
-          
-        elif tipo == 'Reclamação':
-            titulo = input('Digite o titulo da reclamação: ')
+
+            descricao = input("Digite a descrição da manifestação: ")
             print()
             time.sleep(0.5)
-        
-    
-        elif tipo == 'Sugestão':
-            titulo = input('Digite o titulo da sugestão: ')
+            autor = input("Digite o autor da manifestação: ")
             print()
             time.sleep(0.5)
-    
-    
-    
-    descricao = input ('Digite a descrição da manifestação: ')
-    
-    autor = input ('Digite o autor da manifestação: ')
-    sql = "insert into manifestacoes (titulo, descricao, autor, tipo) values (%s, %s, %s, %s)"
-    dados = (titulo, descricao, autor, tipo)
-    insertNoBancoDados(connect,sql, dados)
+            sql = "insert into manifestacoes (titulo, descricao, autor, tipo) values (%s, %s, %s, %s)"
+            dados = (titulo, descricao, autor, tipo)
+            insertNoBancoDados(connect, sql, dados)
+        elif tipo == "Reclamação":
+            titulo = input("Digite o titulo da reclamação: ")
+            print()
+            time.sleep(0.5)
+
+            descricao = input("Digite a descrição da manifestação: ")
+            print()
+            time.sleep(0.5)
+            autor = input("Digite o autor da manifestação: ")
+            print()
+            time.sleep(0.5)
+            sql = "insert into manifestacoes (titulo, descricao, autor, tipo) values (%s, %s, %s, %s)"
+            dados = (titulo, descricao, autor, tipo)
+            insertNoBancoDados(connect, sql, dados)
+        elif tipo == "Sugestão":
+            titulo = input("Digite o titulo da sugestão: ")
+            print()
+            time.sleep(0.5)
+
+            descricao = input("Digite a descrição da manifestação: ")
+            print()
+            time.sleep(0.5)
+            autor = input("Digite o autor da manifestação: ")
+            print()
+            time.sleep(0.5)
+            sql = "insert into manifestacoes (titulo, descricao, autor, tipo) values (%s, %s, %s, %s)"
+            dados = (titulo, descricao, autor, tipo)
+            insertNoBancoDados(connect, sql, dados)
+
+encerrarBancoDados(connect)
