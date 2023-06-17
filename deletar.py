e55ee7b97405ec89da0421d84638b4dd0247d1ac
ramongc08 <ramongc118@gmail.com>
2023-06-16 21:42:35 -0300

    opção deletar, para testes

--- deletar.py
+++ deletar.py
@@ -4,7 +4,7 @@ connect = abrirBancoDados("localhost", "root", "831920", "bdouvidoria")
 
 
 def excluir_bd():
-    codigo = input("Digite seu codigo, para excluir a ocorrencia: ")
+    codigo = input("Digite seu código, para excluir a ocorrência: ")
     sql = "select codigo from manifestacoes"
     resultado = listarBancoDados(connect, sql)
     resultado = str(resultado).strip("[](),")
@@ -12,3 +12,6 @@ def excluir_bd():
         sql = "delete from manifestacoes where codigo =" + codigo
         dados = ()
         excluirBancoDados(connect, sql, dados)
+        print("A ocorrência foi apagada com sucesso!")
+    else:
+        print("Comando inexistente!, tente novamente.")
