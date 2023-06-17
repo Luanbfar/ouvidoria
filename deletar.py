f0e45b66c26bcd760e8a746b70b08f62be4545aa
Luan <luanbfarias@outlook.com>
2023-06-16 21:34:11 -0300

    Nova versão da função deletar

--- deletar.py
+++ deletar.py
@@ -1,12 +1,14 @@
 from operacoesbd import *
+
 connect = abrirBancoDados("localhost", "root", "831920", "bdouvidoria")
+
 
 def excluir_bd():
     codigo = input("Digite seu codigo, para excluir a ocorrencia: ")
     sql = "select codigo from manifestacoes"
-    resultado = listarBancoDados(connect, sql )
+    resultado = listarBancoDados(connect, sql)
     resultado = str(resultado).strip("[](),")
-    if codigo in resultado:   
+    if codigo in resultado:
         sql = "delete from manifestacoes where codigo =" + codigo
         dados = ()
         excluirBancoDados(connect, sql, dados)
