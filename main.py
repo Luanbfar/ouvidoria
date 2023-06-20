84fb0b85582e27d50fcb6129b1b50034494b8777
Luan <134301188+Luanbfar@users.noreply.github.com>
2023-06-19 21:45:10 -0300

    Enviando versão final do código

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from funções import *
-import time
+from funcoes import *
 
 opMenu = -1
 
 connect = abrirBancoDados("localhost", "root", "root", "bdouvidoria")
 
-print("Bem-vindo(a) ao sistema de ouvidoria!")
-print()
-print('Carregando', end='')
-for i in range(3):
-    time.sleep(0.2)
-    print(".", end='')
 
-while opMenu != 5:
+
+print("Bem-vindo(a) ao sistema de ouvidoria!")
+
+while opMenu != 8:
     print()
-    print("Opção 1: Listar")
-    print("Opção 2: Adicionar")
-    print("Opção 3: Excluir")
-    print("Opção 4: Editar")
-    print("Opção 5: Sair")
+    print("Opção 1: Listar as manifestações")
+    print("Opção 2: Listar manifestação por tipo")
+    print("Opção 3: Criar nova manifestação")
+    print("Opção 4: Exibir quatidade de manifestações")
+    print("Opção 5: Pesquisar uma manifestação por código")
+    print("Opção 6: Alterar o Título e Descrição de uma Manifestação")
+    print("Opção 7: Excluir uma Manifestação pelo Código")
+    print("Opção 8: Sair do Sistema")
     print()
 
     opMenu = int(input("Escolha uma opção: "))
@@ -26,17 +25,20 @@ while opMenu != 5:
     if opMenu == 1:
         listar_bd()
     elif opMenu == 2:
-        adicionarFeedback()
+        listarTipo_bd()
     elif opMenu == 3:
-        excluir_bd()
+        add_bd()
     elif opMenu == 4:
-        editar_bd()
+        exibirQnt_bd()
     elif opMenu == 5:
-        print("Saindo do Sistema", end='')
-        for i in range(3):
-            time.sleep(0.2)
-            print(".", end='')
+        pesquisar_bd()
+    elif opMenu == 6:
+        editar_bd()
+    elif opMenu == 7:
+        excluir_bd()
+    elif opMenu == 8:
+        print("Saindo...")
     else:
-        print("Opção inválida.")
+        print("Opção inválida. Digite novamente")
 
 encerrarBancoDados(connect)
