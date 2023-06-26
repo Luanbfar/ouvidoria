eae60602ba085516dc0299830232a52ed599d5f9
talles1x <tallessr123@gmail.com>
2023-06-26 20:42:29 -0300

    codigo 100%atualizado

--- funcoes.py
+++ funcoes.py
@@ -1,48 +1,69 @@
 from operacoesbd import *
 
-connect = abrirBancoDados("localhost", "root", "root", "bdouvidoria")
+connect = abrirBancoDados("localhost", "root", "12345", "bdouvidoria")
 
-def listar_bd(): #função para listar as linhas do banco de dados
+
+def listar_bd():  # função para listar as linhas do banco de dados
     sql = "select * from manifestacoes"
     resultado = listarBancoDados(connect, sql)
-    if len(resultado) == 0: #checa se existem linhas no banco de dados através do método len()
-        print("Sem manifestações cadastradas.")    
+    if len(resultado) == 0:  # checa se existem linhas no banco de dados através do método len()
+        print("Sem manifestações cadastradas.")
     else:
-        for i in resultado: #imprime cada linha separadamente utilizando o hífen para separar os elementos de cada linha
+        for i in resultado:  # imprime cada linha separadamente utilizando o hífen para separar os elementos de cada linha
             print(*i, sep=" - ")
-            
-def listarTipo_bd(): #função para listar as linhas do banco de dados por tipo de manifestação
-    sql = "select * from manifestacoes"
-    resultado = listarBancoDados(connect, sql)
-    if len(resultado) == 0: #checa se existem linhas no banco de dados através do método len()
-        print("Sem manifestações no banco de dados.")
-    else:
-        tipo = input(
-            "Digite o tipo de manifestação a ser listada (Reclamação; Sugestão; Elogio): "
-        ).capitalize()
-        if tipo in ["Reclamação", "Sugestão", "Elogio"]: #checa se o tipo inserido pelo usuário pertence aos tipos válidos
-            sql = "select * from manifestacoes where tipo = '{}'".format(tipo)
-            resultado = listarBancoDados(connect, sql)
-            for i in resultado: #imprime as manifestações com o tipo selecionado separando as colunas com o hífen
-                print(*i, sep=" - ")
-        else:
-            print("Tipo inválido!") #mostra essa mensagem de erro se o tipo inserido for diferente dos tipos válidos
 
-def add_bd(): #função para adicionar manifestação ao banco de dados
+
+def listarTipo_bd():  # função para listar as linhas do banco de dados por tipo de manifestação
+    tipos_validos = ["Reclamação", "Sugestão", "Elogio"]
+    tipo = input("Digite o tipo de manifestação a ser listada (1 - Reclamação; 2 - Sugestão; 3 - Elogio): ")
+
+    if tipo == "1":
+        tipo_selecionado = "Reclamação"
+    elif tipo == "2":
+        tipo_selecionado = "Sugestão"
+    elif tipo == "3":
+        tipo_selecionado = "Elogio"
+    else:
+        print("Opção inválida!")
+        return
+
+    sql = "select * from manifestacoes where tipo = '{}'".format(tipo_selecionado)
+    resultado = listarBancoDados(connect, sql)
+
+    if len(resultado) == 0:
+        print("Sem manifestações desse tipo cadastradas.")
+    else:
+        for i in resultado:
+            print(*i, sep=" - ")
+
+
+def add_bd():
     titulo = input("Digite o título da manifestação: ")
     descricao = input("Digite a descrição da manifestação: ")
     autor = input("Digite o nome do autor da manifestação: ")
-    tipo = input("Digite o tipo de manisfetação: ").capitalize()
-    if tipo not in ["Reclamação", "Sugestão", "Elogio"]: #checa se o tipo inserido pelo usuário pertence aos tipos válidos ou não
-        print("Tipo de manifestação inválida!")
-    else:
-        sql = (
-            "insert into manifestacoes (titulo, descricao, autor, tipo) values (%s, %s, %s, %s)"
-        )
-        dados = (titulo, descricao, autor, tipo)
-        insertNoBancoDados(connect, sql, dados)
-        print("Manifestação criada com sucesso!")
 
+    print("Selecione o tipo de manifestação:")
+    print("1. Reclamação")
+    print("2. Sugestão")
+    print("3. Elogio")
+
+    opcao = input("Digite o número correspondente à opção: ")
+
+    if opcao == "1":
+        tipo = "Reclamação"
+    elif opcao == "2":
+        tipo = "Sugestão"
+    elif opcao == "3":
+        tipo = "Elogio"
+    else:
+        print("Opção inválida!")
+        return
+
+    sql = "INSERT INTO manifestacoes (titulo, descricao, autor, tipo) VALUES (%s, %s, %s, %s)"
+    dados = (titulo, descricao, autor, tipo)
+    insertNoBancoDados(connect, sql, dados)
+
+    print("Manifestação criada com sucesso!")
 def exibirQnt_bd():
     # Cursor para executar consultas SQL
     cursor = connect.cursor()
@@ -78,42 +99,39 @@ def exibirQnt_bd():
     connect.close()
 
 
-def pesquisar_bd(): #função usada para pesquisar manifestações pelo seu código
+def pesquisar_bd():
     codigo = input("Digite o código da manifestação a ser pesquisada: ")
-    sql = "select * from manifestacoes where codigo =" + codigo
+    sql = "select * from manifestacoes where codigo = {}".format(codigo)
     resultado = listarBancoDados(connect, sql)
 
-    if len(resultado) == 0: #checa se o código inserido está no banco de dados
+    if len(resultado) == 0:
         print("Não existe manifestação com o código informado")
     else:
-        for i in resultado: #imprime cada linha com valores das colunas separados pelo hífen
+        for i in resultado:
             print(*i, sep=" - ")
 
-def editar_bd(): #função para editar título e descrição de manifestações
+
+def editar_bd():
     codigo = input("Digite o código da manifestação a ser editada: ")
-    sql = "select codigo from manifestacoes"
+    sql = "SELECT codigo FROM manifestacoes WHERE codigo = {}".format(codigo)
     resultado = listarBancoDados(connect, sql)
-    resultado = str(resultado).strip("[](),")
-    if codigo in resultado: #checa se o código inserido é válido e procede para a próxima etapa
+
+    if resultado:  # Verifica se o resultado não está vazio
         titulo = input("Digite o novo título: ")
         descricao = input("Digite a nova descrição: ")
-        sql = "update manifestacoes set titulo = %s, descricao = %s where codigo = %s"
-        dados = (titulo, descricao, codigo)
-        atualizarBancoDados(connect, sql, dados)
+        sql = "UPDATE manifestacoes SET titulo = %s, descricao = %s WHERE codigo = %s"
+        atualizarBancoDados(connect, sql, (titulo, descricao, codigo))
         print("Manifestação editada com sucesso!")
     else:
         print("Código inválido")
 
 
-def excluir_bd(): #função para excluir manifestação do banco de dados
-    codigo = input("Digite seu código para excluir a ocorrência: ")
-    sql = "select codigo from manifestacoes"
-    resultado = listarBancoDados(connect, sql)
-    resultado = str(resultado).strip("[](),")
-    if codigo in resultado: #checa se o código pertence ao banco de dados e procede para os próximos passos
-        sql = "delete from manifestacoes where codigo =" + codigo
-        dados = ()
-        excluirBancoDados(connect, sql, dados)
+def excluir_bd():  # função para excluir manifestação do banco de dados
+    codigo = input("Digite o código da manifestação a ser excluída: ")
+    sql = "delete from manifestacoes where codigo = {}".format(codigo)
+    resultado = excluirBancoDados(connect, sql, ())
+
+    if resultado:
         print("A ocorrência foi apagada com sucesso!")
     else:
         print("Código inexistente! Tente novamente.")
